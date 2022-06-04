9d3c678164a29a444390b1f6c97fd66c946a7e83
hrdtbs <harada.tsubasa.0422@gmail.com>
2022-06-05 01:27:41 +0900

    feat: Period.dayjs, month, excludes

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,24 @@
 export interface Period {
+  /**
+   * 指定した曜日を含める
+   * 0~6
+   */
   weekdays: number[];
+  /**
+   * 当月以外で生成する
+   * 1~12
+   */
+  month?: number;
+  /**
+   * 指定した日付を含める
+   * 1~31
+   */
+  days?: number[];
+  /**
+   * 指定した日付を除く
+   * 1~31
+   */
+  excludes?: number[];
 }
 
 export type Routes = {
--- utils/genICCardCSV.ts
+++ utils/genICCardCSV.ts
@@ -13,18 +13,29 @@ export const genICCardCSV = async (
   period: Period,
   routes: Routes
 ) => {
-  const firstDay = dayjs().startOf("month");
+  const firstDay = period?.month
+    ? dayjs()
+        .set("month", period.month - 1)
+        .startOf("month")
+    : dayjs().startOf("month");
   const dayCount = firstDay.daysInMonth();
   const rows: string[] = [];
   for (let date = 1; date < dayCount + 1; date++) {
     const day = firstDay.set("date", date);
-    if (period.weekdays.includes(day.day())) {
-      routes.forEach(({ from, to, amount }) => {
-        rows.push(
-          `${day.format("YYYY/MM/DD")},s,s,${from},s,s,${to},${amount},s,s,s`
-        );
-      });
+    if (period.excludes?.includes(date)) {
+      continue;
     }
+    if (period.weekdays.includes(day.day()) === false) {
+      continue;
+    }
+    if (period.days?.includes(date) === false) {
+      continue;
+    }
+    routes.forEach(({ from, to, amount }) => {
+      rows.push(
+        `${day.format("YYYY/MM/DD")},s,s,${from},s,s,${to},${amount},s,s,s`
+      );
+    });
   }
   const data = template(rows.join("\n"));
   await writeFileShiftJIS(filePath, data);
